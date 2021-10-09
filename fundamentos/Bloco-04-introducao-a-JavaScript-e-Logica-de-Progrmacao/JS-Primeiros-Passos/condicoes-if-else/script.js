37bd7e6b0717436fbe80db5708330fc259a5620f
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-09 11:19:13 -0300

    if else

---
+++ fundamentos/Bloco-04-introducao-a-JavaScript-e-Logica-de-Progrmacao/JS-Primeiros-Passos/condicoes-if-else/script.js
@@ -0,0 +1,20 @@
+const notaCand = 80
+
+if (notaCand >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaCand < 80 && notaCand >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else if (notaCand < 60) { 
+    console.log("Você foi reprovada(o)");
+}
+
+
+const a= 40
+const b= 30
+const c= 50
+
+if (a > b ) {
+    console.log(a)
+} else if (c > a) {
+    console.log(c)
+}else console.log(b)
