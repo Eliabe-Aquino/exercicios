f538712a861abe6048b80b697951811245950541
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-15 12:52:11 -0300

    modificacao

--- fundamentos/Bloco-04-introducao-a-JavaScript-e-Logica-de-Progrmacao/dia-3-javascript-logica-de-programacao-e-algoritmos/acript.js
+++ fundamentos/Bloco-04-introducao-a-JavaScript-e-Logica-de-Progrmacao/dia-3-javascript-logica-de-programacao-e-algoritmos/acript.js
@@ -33,3 +33,32 @@ for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
   inputLine+=symbol;
 
 }
+
+
+
+
+
+
+
+
+// 1 - Crie um objeto player contendo as variáveis listadas abaixo.
+
+let person  = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 },
+};
+// 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+console.log('A jogadora ' + person.name + ' ' + person.lastName + ' ' + 'tem ' + person.age + ' ' + 'anos de idade.')
+
+// 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+person.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+
+// 4 - Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
+
+console.log('A jogadora ' + person.name + ' ' + person.lastName + 'foi eleita a melhor do mundo por ' + person.bestInTheWorld.length + ' vezes, nos anos')
+
+// 5 - Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
+
+console.log('A jogadora possui ' + person.medals.golden + ' medalhas de ouro e ' + person.medals.silver + ' medalhas de prata.'  )
