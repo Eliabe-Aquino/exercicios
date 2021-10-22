9d6cccae2d2c12d7f7699540dc0e2a209fcedc64
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-22 11:05:54 -0300

    function

--- fundamentos/Bloco-5-JavaScript-DOMs-Eventos-e-Web-Storage/dia-3-JavaScript-Eventos/main.js
+++ fundamentos/Bloco-5-JavaScript-DOMs-Eventos-e-Web-Storage/dia-3-JavaScript-Eventos/main.js
@@ -12,6 +12,9 @@ const myWebpage = document.getElementById('my-spotrybefy');
 //   transform: translateY(-20px);
 // }
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+function techClick (){
+  
+}
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
