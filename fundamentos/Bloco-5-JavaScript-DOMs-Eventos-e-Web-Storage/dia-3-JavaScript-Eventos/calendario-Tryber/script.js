a83df524110bf6c0b83e9f868eb6022e4b72f264
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-23 15:07:22 -0300

    calendario exercicio 1 done

---
+++ fundamentos/Bloco-5-JavaScript-DOMs-Eventos-e-Web-Storage/dia-3-JavaScript-Eventos/calendario-Tryber/script.js
@@ -0,0 +1,32 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  const listDays = document.getElementById('days')
+  for (let days = 0; days < dezDaysList.length; days += 1 ){
+      let daysDez = document.createElement('li')
+      if(dezDaysList[days] === 4 || dezDaysList[days] === 11 || dezDaysList[days] === 18) {
+        daysDez.className = 'day friday'
+      } else if (dezDaysList[days] === 24 || dezDaysList[days] === 31){
+        daysDez.className = 'day holiday'
+      } else if(dezDaysList[days] === 25){
+        daysDez.className = 'day holiday friday'
+      } else daysDez.className = 'day'
+      daysDez.innerText = dezDaysList[days]
+      listDays.appendChild(daysDez)
+        
+  }
+  
