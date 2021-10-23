d69d5072909bcd290b0903b50014264de8e7828b
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-23 15:21:41 -0300

    Botao adicionado

--- fundamentos/Bloco-5-JavaScript-DOMs-Eventos-e-Web-Storage/dia-3-JavaScript-Eventos/calendario-Tryber/script.js
+++ fundamentos/Bloco-5-JavaScript-DOMs-Eventos-e-Web-Storage/dia-3-JavaScript-Eventos/calendario-Tryber/script.js
@@ -16,6 +16,7 @@ function createDaysOfTheWeek() {
   // Escreva seu código abaixo.
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
   const listDays = document.getElementById('days')
+  
   for (let days = 0; days < dezDaysList.length; days += 1 ){
       let daysDez = document.createElement('li')
       if(dezDaysList[days] === 4 || dezDaysList[days] === 11 || dezDaysList[days] === 18) {
@@ -29,4 +30,15 @@ function createDaysOfTheWeek() {
       listDays.appendChild(daysDez)
         
   }
-  
+
+  function button(btnName){
+    const divButton = document.querySelector('.buttons-container')
+    const button = document.createElement('button')
+    const buttonid = "btn-holiday"
+      
+        button.innerHTML = btnName
+        button.id = buttonid
+        divButton.appendChild(button)
+      
+  }
+  button('Feriados')
