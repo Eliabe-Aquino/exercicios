db794149bb32db474334258ec89c6cdf289583e6
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-28 19:44:11 -0300

    criando tags

---
+++ fundamentos/Bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,13 @@
+const siglasEstados = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "GO", "ES", "MA",
+    "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+    "RS", "RO", "RR", "SP", "SC", "SE", "TO",]
+
+const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa', 'Catarina', 'São Paulo', 'Sergipe',  'Tocantins']
+
+const select = document.getElementById('state-client')
+for(let i = 0; i < 27; i += 1 ){
+    const opcao = document.createElement('option')
+    select.appendChild(opcao)
+    opcao.value = siglasEstados[i]
+    opcao.innerText = estados[i]
+}
