801fe67ebdde2552a6b4ccd8acc808d02850fb56
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-10-28 20:55:08 -0300

    estilizacao

--- fundamentos/Bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/Bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,11 +1,11 @@
-const siglasEstados = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "GO", "ES", "MA",
+const siglasEstados = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
     "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
-    "RS", "RO", "RR", "SP", "SC", "SE", "TO",]
+    "RS", "RO", "RR", "SC", "SP", "SE", "TO",]
 
-const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa', 'Catarina', 'São Paulo', 'Sergipe',  'Tocantins']
+const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe',  'Tocantins']
 
 const select = document.getElementById('state-client')
-for(let i = 0; i < 27; i += 1 ){
+for(let i = 0; i < estados.length; i += 1 ){
     const opcao = document.createElement('option')
     select.appendChild(opcao)
     opcao.value = siglasEstados[i]
