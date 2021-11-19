4287da0f1163bda05967ca3fb03fa55deda6c9c6
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-11-19 15:37:33 -0300

    exercicio mentoria

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -117,19 +117,3 @@ const people = [
 
 // Crie um algoritmo que, ao receber o id numérico de um produto, 
 // retorne uma string com o nome do produto correspondente. 
-const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
-
-const prices = [2.99, 3.99, 1.5, 2];
-
-const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
-  { [product]: listPrices[index] }
-));
-
-const listProducts = updateProducts(products, prices);
-console.log(listProducts);
-// => [
-//   { Arroz: 2.99 },
-//   { Feijao: 3.99 },
-//   { Alface: 1.5 },
-//   { Tomate: 2 },
-// ]
