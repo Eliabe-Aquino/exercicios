1a59c6b22a8fd18582241ccaf01ce13a8bc2a33f
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-11-19 15:37:05 -0300

    exercicio mentoria

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -18,23 +18,118 @@
 // Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
 
 
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+// const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+// const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const repeat = (rightAnswers, studentAnswers, action) => {
-  let contador = 0;
-  for (let index = 0; index < rightAnswers.length; index += 1) {
-    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
-    contador += actionReturn;
-  }
-  return `Resultado final: ${contador} corretas`;
-};
+// const repeat = (rightAnswers, studentAnswers, action) => {
+//   let contador = 0;
+//   for (let index = 0; index < rightAnswers.length; index += 1) {
+//     const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+//     contador += actionReturn;
+//   }
+//   return `Resultado final: ${contador} corretas`;
+// };
 
-console.log(repeat(RIGHT_ANSWERS, STUDENT_ANSWERS, (rAnswer, uAnswer) => {
-  if (rAnswer === uAnswer) {
-    return 1;
-  } if (uAnswer === 'N.A') {
-    return 0;
-  }
-  return -0.5;
-}));
+// console.log(repeat(RIGHT_ANSWERS, STUDENT_ANSWERS, (rAnswer, uAnswer) => {
+//   if (rAnswer === uAnswer) {
+//     return 1;
+//   } if (uAnswer === 'N.A') {
+//     return 0;
+//   }
+//   return -0.5;
+// }));
+
+// const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+// const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+// console.log(verifyFirstLetter('J', listNames)); // true
+// console.log(verifyFirstLetter('x', listNames)); // false
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  // Adicione se código aqui
+  people.sort((a, b) => b.age - a.age)
+  console.log(people);
+
+  const techProducts = [
+    {
+        id: 1, // numérico
+        name: 'computer', // string
+        price: 2100, // number
+    },
+    {
+        id: 2,
+        name: 'mouse',
+        price: 56,
+    },
+    {
+        id: 3,
+        name: 'monitor',
+        price: 589,
+    },
+    {
+        id: 4,
+        name: 'keyboard',
+        price: 78,
+    },
+    {
+        id: 5,
+        name: 'HD',
+        price: 350,
+    },
+    {
+        id: 6,
+        name: 'webcam',
+        price: 187,
+    },
+    {
+        id: 7,
+        name: 'mic',
+        price: 69,
+    },
+    {
+        id: 8,
+        name: 'headset',
+        price: 216,
+    },
+];
+
+// Crie um algoritmo que retorna true se houver algum produto com preço acima de 300 e false caso contrário
+
+// Crie um algoritmo que retorne um array com nome de todos os produtos de ids pares
+
+// Crie um algoritmo que receba o id de um produto e uma quantidade e retorne o preço total
+// ex: id: 8 e quantidade: 2 preço total: 432
+
+// Crie um algoritmo que retorne a quantidade de produtos com ids impares cadastrados 
+
+// Crie uma função que recebe o valor total de uma compra, aplique desconto de 
+// 20% ao total caso seja maior que 500 e retorno o valor da compra com desconto
+
+// Crie um algoritmo que encontre produtos com id impar e retorne um novo array contendo esses produtos
+
+// Crie um algoritmo que, ao receber o id numérico de um produto, 
+// retorne uma string com o nome do produto correspondente. 
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
