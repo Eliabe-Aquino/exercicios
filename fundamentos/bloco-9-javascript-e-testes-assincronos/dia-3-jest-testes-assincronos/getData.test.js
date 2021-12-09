c4df23689894e9afb28abf310783f2b73680d622
Eliabe Aquino <eliabe.tavares@hotmail.com>
2021-12-09 18:24:37 -0300

    ajustando

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/getData.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/getData.test.js
@@ -1,5 +1,4 @@
 const getSuperHero = require('./getData');
-
 test('Verifica se o nome do herói é Wonder Woman', async () => {
   const hero = await getSuperHero('https://www.superheroapi.com/api.php/4192484924171229/720');
   expect(hero).toBe('Wonder Woman');
